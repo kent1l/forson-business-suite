f6154b51a3b8f0a2883e484f451ef3589d70f272
kent1l <kent1l.pilar@gmail.com>
2026-09-21 09:42:24 +0000

    chore: remove temporary patch and test scripts

--- patch.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('packages/api/routes/stagedSaleRoutes.js', 'utf8');
-code = code.replace(
-  "console.error('Error approving staged sale:', err.message);",
-  "console.error('Error approving staged sale:', err.stack);"
-);
-fs.writeFileSync('packages/api/routes/stagedSaleRoutes.js', code);
--- patch3.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('packages/api/tests/stagedSales.test.js', 'utf8');
-code = code.replace(
-  "expect(res.status).toBe(200);",
-  "console.log('--- CLIENT QUERY CALLS ---'); console.log(client.query.mock.calls.map(c => c[0].substring(0, 50))); expect(res.status).toBe(200);"
-);
-fs.writeFileSync('packages/api/tests/stagedSales.test.js', code);
